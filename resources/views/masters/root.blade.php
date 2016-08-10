ed056dfa55c88da98ac271b3b8c76023e4689a10
Throckmorton <tylerthrock95@gmail.com>
2016-08-10 14:44:07 -0400

    Updated master template - scripts, div for body content, and copyright p in footer

--- resources/views/masters/root.blade.php
+++ resources/views/masters/root.blade.php
@@ -5,6 +5,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
         @yield('scripts')
+        <script src="/js/base.js" type="text/javascript"></script>
+        <script src="/js/navBar.js" type="text/javascript"></script>
         <link rel="stylesheet" href="/css/base.css" type="text/css" />
         <link rel="stylesheet" href="/css/navBar.css" type="text/css" />
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" />
@@ -15,11 +17,14 @@
     <body>
         <div class="rootContainer">
             @include('includes.navBar_include')
-            @yield('contentViewer')
-            @yield('bodyContent')
+            <div class="body">
+                @yield('bodyContent')
+            </div>
+
         </div>
         <footer>
             @yield('footer')
+            <p id="copyright"></p>
         </footer>
     </body>
 </html>
