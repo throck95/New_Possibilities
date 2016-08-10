0f37cecbba8d2461204a7703dc7c9a06b924d9e1
Throckmorton <tylerthrock95@gmail.com>
2016-08-10 14:49:09 -0400

    Updated navBar logo and designed using UL

--- resources/views/includes/navBar_include.blade.php
+++ resources/views/includes/navBar_include.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div id="headerBar"></div>
     <div id="logoBar">
-        <a href="{{URL::route('home')}}" title="New Possibilities"><img src="/images/new-possibilities_250.png" /></a>
+        <a href="{{URL::route('home')}}" title="New Possibilities"><img src="/images/logo_placeholder.png" /></a>
         <p class="contactUsHeader">
             <strong>Office Location: </strong>Cincinnati<br />
             <strong>Phone: </strong>(555) 555-5555
@@ -10,14 +10,25 @@
     @yield('header')
 </header>
 <div id="navBar">
-    <div id="navButtonsDiv">
-        <button class="navButton" id="resultsNavButton">Counseling</button>
-        <button class="navButton" id="projectsNavButton">Coaching</button>
-        <button class="navButton" id="templatesNavButton">Our Team</button>
-        <button class="navButton" id="templatesNavButton">What to Expect</button> <!-- not sure about reality of this page -->
-        <button class="navButton" id="emailNavButton">Contact Us</button>
-    </div>
-    <div id="navInfoDiv">
-        <p>Nav info here</p>
-    </div>
+    <ul id="navButtonsList">
+        <li><a class="@yield('homeClassDefault')" href="{{URL::route('home')}}">Home</a></li>
+        <li class="navLi counselingNav">
+            <a class="navButton @yield('counselingClassDefault')" href="{{URL::route('counseling')}}">Counseling</a>
+            <div class="navContent">
+                <a class="@yield('relationshipClassDefault')" href="{{URL::route('relationships')}}">Marriage and Relationships</a>
+                <a class="@yield('stressClassDefault')" href="{{URL::route('stress')}}">Stress and Anxiety</a>
+                <a class="@yield('substanceClassDefault')" href="{{URL::route('substance')}}">Substance Use</a>
+            </div>
+        </li>
+        <li class="navLi coachingNav">
+            <a class="navButton @yield('coachingClassDefault')" href="{{URL::route('coaching')}}">Coaching</a>
+            <div class="navContent">
+                <a class="@yield('lifePhaseTransitionsClassDefault')" href="{{URL::route('lifePhaseTransitions')}}">Life Phase Transitions</a>
+                <a class="@yield('profGrowthClassDefault')" href="{{URL::route('profGrowth')}}">Professional Growth</a>
+            </div>
+        </li>
+        <li><a class="@yield('teamClassDefault')" href="{{URL::route('team')}}">Our Team</a></li>
+        <li><a class="@yield('expectationClassDefault')" href="{{URL::route('expectation')}}">What to Expect</a></li>
+        <li><a class="@yield('contactClassDefault')" href="{{URL::route('contact')}}">Contact Us</a></li>
+    </ul>
 </div>
