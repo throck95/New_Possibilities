02b62f99f0269f4aa2c174d7f38fc45ab7174b53
Throckmorton <tylerthrock95@gmail.com>
2016-08-11 12:17:26 -0400

    Added attributions page for CC links

---
+++ resources/views/pages/attributions.blade.php
@@ -0,0 +1,10 @@
+@extends('masters.root')
+@section('title')
+    New Possibilities - Attributions
+@stop
+
+@section('bodyContent')
+    <ol>
+        <li><a href="https://creativecommons.org/licenses/by/4.0/">"Don't crack under pressure"</a></li>
+    </ol>
+@stop
